d9456d98d3209e017b202289da7e205667ea9ad7
10sr <8slashes+git@gmail.com>
2016-06-16 15:52:12 +0900

    Fix logger name

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 module.exports = function(robot){
   //robot.send({}, "おやすみ");
-  robot.log.info("Deploy done.");
+  robot.logger.info("Deploy done.");
   robot.respond(/ping/, function(res){
     res.send("うっさい");
   });
